c8835d41be931d33b25c467d575e1de9d423171a
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2022-12-14 11:13:13 +0700

    Create Transaksi.java

---
+++ Transaksi.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+
+public class Transaksi{
+    ArrayList<Makanan> Makanan;
+    DecimalFormat df = new DecimalFormat("#.000");
+
+    public Transaksi(){
+        this.Makanan = new ArrayList<Makanan>();
+    }
+    
+    
+}
